1c16d5f5cf65ce46ec716bec17d73598715c4a8f
Nathan <antnefedov33@gmail.com>
2023-12-06 12:55:57 +0300

    made basic variables, random color and random number generating functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,35 @@
 "use strict"
+
+let canvas = document.querySelector('#canvas');
+let ctx = canvas.getContext('2d');
+
+let canvasWidth;
+let canvasHeight;
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+function init() {
+    sizeCanvas();
+}
+
+function sizeCanvas() {
+    canvasWidth = canvas.width = window.innerWidth;
+    canvasHeight = canvas.height = window.innerHeight;
+}
+
+window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("resize", sizeCanvas);
+
+function createRGB() {
+    let red = getRandomNumber(0, 255);
+    let green = getRandomNumber(0, 255);
+    let blue = getRandomNumber(0, 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+};
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
